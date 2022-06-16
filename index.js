823d96447c570dc6b64e4f351ebb1f52701fa98c
Mwenje <mwenje24@gmail.com>
2022-06-16 12:09:58 +0300

    done assignment

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-// Add your code here
+let name = "Sam"
+let email = "sam@sam.com"
+
+function submitData( name, email ) {
+    return fetch( 'http://localhost:3000/users', {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json"
+        },
+        body: JSON.stringify( {
+            name,
+            email
+        } )
+    } )
+    .then(res => res.json())
+    .then(data => {
+        console.log(data)
+        document.body.innerHTML = data[ "id" ]
+    })
+    .catch( function ( error ) {
+    document.body.innerHTML = error.message
+    } )
+}
